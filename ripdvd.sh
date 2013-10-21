989fd7f9008875373a6a2beb2068c248ba7db4ee
rdkaiser <ryandkaiser@gmail.com>
2013-10-21 10:59:09 -0500

    Update ripdvd.sh

--- ripdvd.sh
+++ ripdvd.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-HANDBRAKE_CLI='/usr/bin/HandBrakeCLI'
+$ECHO='/bin/echo'
+$SED='/bin/sed'
+$AWK='/usr/bin/awk'
+
 DVD_INFO='/usr/bin/cd-info -dvd'
+HANDBRAKE_CLI='/usr/bin/HandBrakeCLI'
 EXT='m4v'
 
+INDEV=$( $DVD_INFO | grep 'CD location' | $AWK -F ':' '{print $2}' | $SED 's/ //' )
 
-INDEV=$( $DVD_INFO | grep 'CD location' | awk -F ':' '{print $2}' | sed 's/ //' )
-
-echo -e -n  "Enter a destination directory: "
+$ECHO -e -n  "Enter a destination directory: "
 read VID_DIR
 
-echo -e -n "\nEnter a title for the file to be encoded: "
+$ECHO -e -n "\nEnter a title for the file to be encoded: "
 read TITLE
 
-echo -e -n "\nEnter the handbrake preset you wish to apply: "
+$ECHO -e -n "\nEnter the handbrake preset you wish to apply: "
 read PRESET
 
-echo -e "\nDoes everything look correct? [Y/n] \n"
-echo -n " $HANDBRAKE_CLI -i $INDEV -o $VID_DIR/$TITLE.$EXT -Z $PRESET "
+$ECHO -e "\nDoes everything look correct? [Y/n] \n"
+$ECHO -n " $HANDBRAKE_CLI -i $INDEV -o $VID_DIR/$TITLE.$EXT -Z $PRESET "
 read q
 
 if [ $q == 'y' ]; then
-	echo "OK. Encoding file..."
+	$ECHO "OK. Encoding file..."
 	$HANDBRAKE_CLI -i $INDEV -o $VID_DIR/$TITLE.$EXT -Z $PRESET
 else
-	echo "Exiting..."
+	$ECHO "Exiting..."
 	exit 1
 fi
 
 exit 0
+
 
